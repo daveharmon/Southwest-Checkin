bb0a6e2dba3c6bb994375af42a2a10d8c30ee76e
colacadstink <benharris5@gmail.com>
2017-11-08 23:28:21 -0500

    Changed code to use the mobile website instead of the desktop version - hopefully it'll change/break less often. Also added the ability to send an email with boarding position when the script completes.

--- southwest_checkin.py
+++ southwest_checkin.py
@@ -9,6 +9,10 @@ from datetime import datetime
 from datetime import timedelta
 from time import sleep
 
+import os
+
+sendmail_location = "/usr/sbin/sendmail" # sendmail location; this probably won't change
+
 parser = OptionParser()
 parser.add_option("-c", "--confirmation_num", action="store", dest="conf_num",
                   help="confirmation number for flight", type="string")
@@ -17,9 +21,15 @@ parser.add_option("-f", "--first_name", action="store", dest="first_name",
 parser.add_option("-l", "--last_name", action="store", dest="last_name",
                   help="last name for flight", type="string")
 parser.add_option("-t", "--time", action="store", dest="time",
-				  help="time to start checkin attempts", type="string")
+                  help="time to start checkin attempts", type="string")
 parser.add_option("-H", "--headless", action="store_true", dest="headless",
-				  help="set to run headless and simulate a display")
+                  help="set to run headless and simulate a display")
+parser.add_option("-e", action="store_true", dest="email",
+                  help="set to send an email; use --email-from and --email-to as well")
+parser.add_option("--email-from", action="store", dest="email_from",
+                  help="email address to send from", type="string")
+parser.add_option("--email-to", action="store", dest="email_to",
+                  help="email address to send to", type="string")
 
 (options, args) = parser.parse_args()
 
@@ -35,12 +45,18 @@ driver = webdriver.Chrome()
 wait = WebDriverWait(driver,10)
 
 # go directly to the checkin page
-driver.get("https://www.southwest.com/flight/retrieveCheckinDoc.html")
+driver.get("https://mobile.southwest.com/check-in")
 
 # get text boxes for check in
-confirmation_num = driver.find_element_by_id("confirmationNumber")
-first_name = driver.find_element_by_id("passengerFirstName")
-last_name = driver.find_element_by_id("passengerLastName")
+while True:
+	try:
+		confirmation_num = driver.find_element_by_name("recordLocator")
+		first_name = driver.find_element_by_name("firstName")
+		last_name = driver.find_element_by_name("lastName")
+		break;
+	except NoSuchElementException:
+		print("loading checkin...")
+		sleep(0.1)
 
 # insert data
 confirmation_num.send_keys(options.conf_num)
@@ -65,37 +81,57 @@ while des_time > cur_time:
 
 # check in!
 print("checking in...")
-driver.find_element_by_id("form-mixin--submit-button").click()
+submit = driver.find_element_by_class_name("button--yellow");
+submit.click()
 
-# while an error message exists, keep trying!
+# wait while the loading spinner is up
 try:
-	while driver.find_element_by_class_name("message-error"):
-		print("error displayed")
+	while driver.find_element_by_class_name("dimmer").is_displayed():
+		print("loading...")
 		sleep(0.1)
-		submit.click()
 except NoSuchElementException:
-        print("no error displayed, moving on!")
+        print("no spinner found, moving on - hope everything's okay...")
+
+while True:
+	# while an error message exists, keep trying!
+	try:
+		while driver.find_element_by_class_name("popup-showing"):
+			print("error displayed, trying again")
+			sleep(0.1)
+			driver.find_element_by_class_name("confirm-button").click()
+			sleep(0.5)
+			submit.click()
+	except NoSuchElementException:
+	        print("no error displayed, moving on!")
+		break;
 
 # confirm checkin
-while True:
-	try:
-		checkInConfirm = driver.find_element_by_class_name('submit-button')
-		while checkInConfirm.get_attribute("disabled") is not None:
-			try:
-				print("waiting to click confirm button")
-				sleep(0.1)
-				checkInConfirm = driver.find_element_by_class_name('submit-button')
-			except WebDriverException:
-				# this sometimes fails, give it a moment and try again
-				sleep(0.1)
-				checkInConfirm = driver.find_element_by_class_name('submit-button')
-		checkInConfirm.click()
-		break;
-	except NoSuchElementException, StaleElementReferenceException:
-		print("submit button not found, retrying")
+print("confirming check in...")
+submit = driver.find_element_by_class_name("button--yellow");
+submit.click()
+
+# wait while the loading spinner is up
+try:
+	while driver.find_element_by_class_name("dimmer").is_displayed():
+		print("loading...")
 		sleep(0.1)
+	print("loading done")
+except NoSuchElementException:
+        print("no spinner found, moving on - hope everything's okay...")
+
 print("checked in - getting boarding position")
-boardingPositionDiv = driver.find_element_by_class_name("air-check-in-passenger-item--information-boarding-position")
-boardingPositionSpan = boardingPositionDiv.find_element_by_class_name("swa-g-screen-reader-only")
-position = boardingPositionSpan.get_attribute('innerHTML')
-print(position)
+boardingPositionDiv = driver.find_element_by_class_name("boarding-group-and-position")
+(groupSpan, positionSpan) = boardingPositionDiv.find_elements_by_class_name("xxxlarge")
+group = groupSpan.text
+position = positionSpan.text
+print("Checked in! Boarding position "+group+" "+position+".")
+
+# Send mail, if enabled
+if options.email:
+	p = os.popen("%s -t" % sendmail_location, "w")
+	p.write("From: %s\n" % options.email_from)
+	p.write("To: %s\n" % options.email_to)
+	p.write("Subject: Southwest Checkin for %s\n" % options.conf_num)
+	p.write("\n") # blank line separating headers from body
+	p.write("You have been checked in for your flight with confirmation number %s, with boarding position %s." % (options.conf_num, group+" "+position))
+	status = p.close()
